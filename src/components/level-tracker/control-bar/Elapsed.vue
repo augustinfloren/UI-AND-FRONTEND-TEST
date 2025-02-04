6d9e674422c2824fb00279acd2d0c31918d396fc
Augustin Floren <augustinfloren@gmail.com>
2025-02-05 00:19:37 +0100

    feat(level-tracker): add elapsed

---
+++ src/components/level-tracker/control-bar/Elapsed.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div class="elapsed">
+        00:00
+    </div>
+</template>
+
+<style scoped lang="scss">
+@use "@/styles/base/space";
+@use "@/styles/base/typography";
+
+.elapsed{
+    padding-left: space.$space-fixed-double;
+    font-size: typography.$font-size-4;
+}
+</style>
