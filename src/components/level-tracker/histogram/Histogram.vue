f2d9901985f66926ba44905799e3cbb50b989d53
Augustin Floren <augustinfloren@gmail.com>
2025-02-06 18:00:38 +0100

    feat(histogram): histogram update

--- src/components/level-tracker/histogram/Histogram.vue
+++ src/components/level-tracker/histogram/Histogram.vue
@@ -4,81 +4,47 @@ import { onMounted, ref, watch } from "vue";
 import * as d3 from "d3";
 
 const histogram = ref<SVGSVGElement | null>(null);
-// Quand la lecture démarre, démarrer un compteur 
-// et envoyer les données 
-//  : Poser un watcher sur la ref playing et faire quelque chose
-// Créer une data dynamique avec les valeurs du compteur et des données de l'audioplayer
-//  : svg.enter { merge(path) }
-// Quand la lecture est stopée arreter le compteur
-
-let totalElapsed = 0;
+const data = ref<{ time: number | null, level: number | null }[]>([]);
 let lastElapsed = 0;
-
-watch(() => store.playing, () => {
-    if (store.playing) {
-        let t = d3.timer((elapsed) => {
-            totalElapsed = lastElapsed + elapsed;
-            console.log(totalElapsed)
-            if (!store.playing) {
-                t.stop();
-                lastElapsed = totalElapsed;
-            } 
-        }, 150);
-    }
-});
-
-const data: {time: number, level: number} [] = [
-    {time: 0, level : 50}, 
-    {time: 5, level : 60},
-    {time: 10, level : 80},
-    {time: 15, level : 80}, 
-    {time: 20, level : 90},
-    {time: 25, level : 80},
-    {time: 30, level : 70},
-    {time: 35, level : 60},
-    {time: 40, level : 80},
-    {time: 45, level : 90},
-    {time: 50, level : 80},
-    {time: 55, level : 70},
-    {time: 60, level : 114},
-]
 
 const width  = 368
 const height = width
 const innerRadius = 50
 const outerRadius = 180
 
-
 onMounted(() => {
-    const svg = d3.select(histogram.value)  // Création du svg avec sa taille
+    const svg = d3.select(histogram.value)  
         .attr("width", width)
         .attr("height", height)
         .style("background", "grey")
         .attr("viewBox", [-width / 2, -height / 2, width, height]) 
-        .attr("style", "width: 100%; height: auto; font: 10px sans-serif;")
+        .attr("style", "width: 100%; height: auto;")
         .attr("stroke-linejoin", "round")
         .attr("stroke-linecap", "round");
     
-    const x = d3.scaleLinear()  
-        .domain([0, 60])  // Echelle des données
-        .range([0, 2 * Math.PI]);  // Longueur de l'anneau 
+    const x = d3.scaleLinear() // TIME
+        .domain([0, 60])  // SCALE
+        .range([0, 2 * Math.PI]);  // RADIAL WIDTH
     
-    const y = d3.scaleLinear()
-        .domain([0, 100])  // Echelle des données 
-        .range([innerRadius, outerRadius]);  // Largeur de l'anneau en px
+    const y = d3.scaleLinear() // LEVEL
+        .domain([0, 60])  // SCALE
+        .range([innerRadius, outerRadius]);  // RADIAL HEIGHT
     
-    const line = d3.lineRadial<[number, number]>() // Définition de la ligne
+    const line = d3.lineRadial<[number, number]>() 
         .curve(d3.curveLinear)
         .angle(d => x(d[0]))   
     
-    svg.append("path")
+    const path = svg.append("path")
         .attr("fill", "none")
         .attr("stroke", "steelblue")
         .attr("stroke-width", 1.5)
         .attr("d", line(
-            data.map(d => [d.time, d.level]) 
+            data.value
+                .filter(d => d.time !== null && d.level !== null)  
+                .map(d => [d.time!, d.level!])
         ));
-            
+           
+    // TICKS
     svg.append("g")
         .selectAll()
         .data(y.ticks())
@@ -88,6 +54,32 @@ onMounted(() => {
                 .attr("stroke", "currentColor")
                 .attr("stroke-opacity", 0.2)
                 .attr("r", y));
+    
+    // HISTOGRAM UPDATE - TO REFACTO
+    watch(() => store.playing, () => {
+        if (store.playing) {
+            let t = d3.timer((elapsed) => {
+                store.elapsedTime = lastElapsed + elapsed;
+    
+                data.value.push(
+                    { time: store.elapsedTime/1000, level: 50 }
+                );
+                
+                path
+                    .transition()  
+                    .duration(500) 
+                    .attr("d", line(
+                        data.value
+                            .filter(d => d.time !== null && d.level !== null) 
+                            .map(d => [d.time!, d.level!])
+                    ));
+                if (!store.playing) {
+                    t.stop();
+                    lastElapsed = store.elapsedTime;
+                } 
+            }, 150);
+        }
+    });
 })
 </script>
 
